09b726983c33c6930f8c7635ed868029e3ed47ce
yanisgallouze <yanisgall77@gmail.com>
2024-09-24 15:24:58 +1100

    Initialisation du package json

---
+++ server-http.mjs
@@ -0,0 +1,14 @@
+import http from "node:http";
+
+const host = "localhost";
+const port = 8000;
+
+function requestListener(_request, response) {
+  response.writeHead(200);
+  response.end("<html><h1>My first server!<h1></html>");
+}
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
