0d2a2b47c6fa2b8a3066d98a571243966f96a178
yanisgallouze <yanisgall77@gmail.com>
2024-09-25 13:21:34 +1100

    Question 1.2 : liste des en-têtes HTTP qui ont changé

--- server-http.mjs
+++ server-http.mjs
@@ -4,8 +4,8 @@ const host = "localhost";
 const port = 8000;
 
 function requestListener(_request, response) {
-  response.writeHead(200);
-  response.end("<html><h1>My first server!<h1></html>");
+  response.setHeader("Content-Type", "application/json");
+  response.end(JSON.stringify({ message: "I'm OK" }));
 }
 
 const server = http.createServer(requestListener);
